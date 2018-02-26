7c4e4685f188266ce85204413a7a0e76b76016d1
davidverdejobluekiri <david.verdejo@bluekiri.com>
2018-02-26 18:11:41 +0100

    add " to numeric values

--- main.tf
+++ main.tf
@@ -14,24 +14,24 @@ resource "google_dataproc_cluster" "mycluster" {
         #staging_bucket        = "dataproc-staging-bucket"
 
         master_config {
-            num_instances     = ${var.master_num_instances}
+            num_instances     = "${var.master_num_instances}"
             machine_type      = "${var.master_machine_type}"
             disk_config {
-                boot_disk_size_gb = ${var.master_boot_disk_size_gb}
+                boot_disk_size_gb = "${var.master_boot_disk_size_gb}"
             }
         }
 
         worker_config {
-            num_instances     = ${var.worker_num_instances}
+            num_instances     = "${var.worker_num_instances}"
             machine_type      = "${var.worker_machine_type}"
             disk_config {
-                boot_disk_size_gb = ${var.worker_boot_disk_size_gb}
-                num_local_ssds    = ${var.worker_num_local_ssds}
+                boot_disk_size_gb = "${var.worker_boot_disk_size_gb}"
+                num_local_ssds    = "${var.worker_num_local_ssds}"
             }
         }
 
         preemptible_worker_config {
-            num_instances     = ${var.preemptible_num_instances}
+            num_instances     = "${var.preemptible_num_instances}"
         }
 
         # Override or set some custom properties
