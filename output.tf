a0e9b14315464f352549523da9e18b71c3fd8555
davidverdejobluekiri <david.verdejo@bluekiri.com>
2018-02-27 09:28:36 +0100

    try output

--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
 output "dataproc_master_instance_names" {
     description = "List of master instance names which have been assigned to the cluster"
-    value = ["${google_dataproc_cluster.this.cluster_config.master_config.instance_names}"]
+    value = ["${google_dataproc_cluster.this.cluster_config.master_config.*.instance_names}"]
 }
